0587baf68b81f3fb33d026919846ba614cbcb501
ibraphem <ibraphem@gmail.com>
2022-11-08 16:20:36 +0100

    clean up

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,7 +2,6 @@ import {useEffect} from 'react';
 import Hero from '../components/hero/Hero';
 import PublicHeader from '../layout/header/PublicHeader';
 import Properties from '../layout/apartment/Properties';
-import { rentData } from '../mock/rentData';
 import "../assets/css/publicStyles/app.css"
 import { useDispatch } from 'react-redux';
 import { getProperties, updatePagination } from '../redux/slices/propertyListingSlice';
